3e4313b4b37176025a28f1393fb82b4650a10b7d
Brent McSharry <brentm@adhb.govt.nz>
2019-03-19 08:52:34 +1300

    dom included in secondary tsconfig

--- test/db/getDrugs.ts
+++ test/db/getDrugs.ts
@@ -1,5 +1,11 @@
 import { expect } from 'chai';
 import { drugsDBLocal } from '../../src/db/injectableImplementations/drugsLocalDb'
+
+
+//Dexie.dependencies.indexedDB = require('fake-indexeddb')
+//Dexie.dependencies.IDBKeyRange = require('fake-indexeddb/lib/FDBKeyRange')
+
+
 describe('MyAppDatabase',()=>{
     it('can instantiate 0 args',()=>{
         
