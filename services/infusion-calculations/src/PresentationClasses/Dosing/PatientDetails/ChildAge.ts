8ac455ca47e6555e800b5f1c9397acc3ce6c53dc
Brent McSharry <brentm@adhb.govt.nz>
2019-04-11 13:57:55 +1200

    comit childage b4 refactor vue

--- services/infusion-calculations/src/PresentationClasses/Dosing/PatientDetails/ChildAge.ts
+++ services/infusion-calculations/src/PresentationClasses/Dosing/PatientDetails/ChildAge.ts
@@ -1,45 +1,128 @@
-import { NumericRange } from './../../../Utilities/NumericRange';
+import { NumericRange } from '../../../Utilities/NumericRange';
 import { IDateProvider, MockableDate } from './MockableDate';
-const daysPerYear: number = 365.25;
-const daysPerMonth: number = daysPerYear / 12;
+const msPerDay = 86400000; // 1000*60*60*24;
+
+const daysPerYear = 365.25;
+const daysPerMonth = daysPerYear / 12;
+const maxAgeYr = 122;
+export { maxAgeYr };
 
 export class ChildAge {
   private static getTotalDays(years: number, months?: number, days?: number): number {
     return years * daysPerYear + (months || 0) * daysPerMonth + (days || 0);
   }
 
-  constructor(readonly years: number, readonly months?: number, readonly days?: number, protected readonly dateProv: IDateProvider = new MockableDate()) {
-    if (years < 0 || months! < 0 || days! < 0) {
-      throw new Error('years, months and days must all be positive');
-    }
-    this.days = days;
-    this.months = months;
-    this.years = years;
+  private pYears?: number;
+  private pMonths?: number;
+  private pDays?: number;
+  private pDob?: Date;
 
-    if (this.days! > 28) {
-      const workingDate = new Date(dateProv.today);
-      let daysInPriorMonth = MockableDate.daysInPriorMonth(workingDate);
-      while (this.days! >= daysInPriorMonth) {
-        this.days = this.days! - daysInPriorMonth;
-        this.months = (this.months || 0) + 1;
-        workingDate.setMonth(workingDate.getMonth() - 1);
-        daysInPriorMonth = MockableDate.daysInPriorMonth(workingDate);
+  constructor(protected readonly dateProv: IDateProvider = new MockableDate()) {}
+
+  public get years() { return this.pYears; }
+  public set years(years: number | undefined) {
+    if (years !== this.pYears) {
+      if (years !== void 0) {
+        if (years < 0) {
+          throw new Error('years must be positive');
+        }
+        if (years > maxAgeYr) {
+          throw new Error('years must be ≤' + maxAgeYr);
+        }
+        years = Math.floor(years);
       }
-    }
-    if (this.months! > 12) {
-      this.years = this.years + (this.months! / 12);
-      this.months = this.months! % 12;
+      this.pYears = years;
+      this.pDob = void 0;
     }
   }
 
-  get totalMonthsEstimate(): number {
+  public get months() { return this.pMonths; }
+  public set months(months: number | undefined) {
+    if (months !== this.pMonths) {
+      if (months !== void 0) {
+        if (months < 0) {
+          throw new Error('months must be positive');
+        }
+        if (this.years === void 0) {
+          this.years = 0;
+        }
+        if (months > 12) {
+          this.years = this.years + (months / 12);
+          months = months % 12;
+        }
+        months = Math.floor(months);
+      }
+      this.pMonths = months;
+      this.pDob = void 0;
+    }
+  }
+
+  public get days() { return this.pDays; }
+  public set days(days: number | undefined) {
+    if (this.pDays !== days) {
+      if (days !== void 0) {
+        if (this.months === void 0) {
+          this.months = 0;
+        }
+        if (days < 0) {
+          throw new Error('days must be positive');
+        }
+        if (days > 28) {
+          const workingDate = new Date(this.dateProv.now());
+          let daysInPriorMonth = MockableDate.daysInPriorMonth(workingDate);
+          while (days >= daysInPriorMonth) {
+            days = days - daysInPriorMonth;
+            this.months = (this.months || 0) + 1;
+            workingDate.setMonth(workingDate.getMonth() - 1);
+            daysInPriorMonth = MockableDate.daysInPriorMonth(workingDate);
+          }
+        }
+      }
+      this.pDays = days;
+      this.pDob = void 0;
+    }
+  }
+
+  public get dob() { return this.pDob; }
+  public set dob(dob: Date | undefined) {
+    if (dob !== void 0 && dob !== this.pDob) {
+      const now = this.dateProv.now();
+      if (dob > now) { throw new Error('DOB must not be AFTER current system date'); }
+      let years = now.getFullYear() - dob.getFullYear();
+      let months = now.getMonth() - dob.getMonth();
+      let days = now.getDate() - dob.getDate();
+      if (months < 0) { months += 12; }
+      if (days < 0) {
+        days += this.dateProv.daysInPriorMonth();
+        if (months === 0) {
+          months = 11;
+          years--;
+        } else {
+          months--;
+        }
+      }
+      const workingDate = new Date(now);
+      workingDate.setFullYear(workingDate.getFullYear() - years);
+      if (dob > workingDate) { years--; }
+      this.pYears = years;
+      this.pMonths = months;
+      this.pDays = days;
+    }
+    this.pDob = dob;
+  }
+
+  public totalMonthsEstimate() {
+    if (this.years === void 0 || (this.years === 0 && this.months === void 0)) {
+      return NaN;
+    }
     return 12 * this.years + (this.months === void 0 ? 6 : this.months);
   }
-  get totalMonths(): number | null {
-    if (typeof this.months === void 0) {
-      return null;
+
+  public totalMonths() {
+    if (this.months === void 0 || this.years === void 0) {
+      return NaN;
     }
-    return 12 * this.years + this.months!;
+    return 12 * this.years + this.months;
   }
 
   public toString(): string {
@@ -51,28 +134,41 @@ export class ChildAge {
     }
     return `${this.years} years ${this.months} months ${this.days} days`;
   }
+
   public toShortString(): string {
     return (this.months !== void 0)
       ? `${this.years} y ${this.months} m`
       : `${this.years} y`;
   }
+
   public getAgeRangeInDays(): NumericRange {
     const numericRange = new NumericRange();
-    if (this.months !== void 0 && this.days !== void 0) {
-      numericRange.lowerBound = numericRange.upperBound = ChildAge.getTotalDays(this.years, this.months, this.days);
-    } else if (this.months !== void 0) {
-      numericRange.lowerBound = ChildAge.getTotalDays(this.years, this.months, 0);
-      const today = new Date();
-      const daysInMonth = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
-      numericRange.upperBound = ChildAge.getTotalDays(this.years, this.months as number, daysInMonth);
-    } else {
-      numericRange.lowerBound = ChildAge.getTotalDays(this.years, 0, 0);
-      numericRange.upperBound = ChildAge.getTotalDays(this.years, 11, 31);
+    if (this.years !== void 0) {
+      if (this.months !== void 0 && this.days !== void 0) {
+        numericRange.lowerBound = numericRange.upperBound = this.getAgeInDays();
+      } else if (this.months !== void 0) {
+        numericRange.lowerBound = ChildAge.getTotalDays(this.years, this.months, 0);
+        const today = new Date();
+        const daysInMonth = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+        numericRange.upperBound = ChildAge.getTotalDays(this.years, this.months as number, daysInMonth);
+      } else {
+        numericRange.lowerBound = ChildAge.getTotalDays(this.years, 0, 0);
+        numericRange.upperBound = ChildAge.getTotalDays(this.years, 11, 31);
+      }
     }
     return numericRange;
   }
-  public getAgeInDays(): number | null {
+
+  public getAgeInDays() {
+    if (this.years === void 0) {
+      return NaN;
+    }
+    if (this.dob !== void 0) {
+      const differenceMs = this.dateProv.now().getTime() - this.dob.getTime();
+      const days = Math.floor(differenceMs / msPerDay);
+      // Convert back to days and return
+      return days;
+    }
     return ChildAge.getTotalDays(this.years, this.months, this.days);
   }
 }
-
