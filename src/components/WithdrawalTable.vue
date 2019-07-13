e9692e52d17a4ddcbca0df5008fde7dd2a9bad23
Brent McSharry <2456704+mcshaz@users.noreply.github.com>
2019-07-13 15:43:49 +1200

    weaning intro display

--- src/components/WithdrawalTable.vue
+++ src/components/WithdrawalTable.vue
@@ -4,28 +4,28 @@
             <h5>ORIGINAL MEDICINE</h5>
             <dl>
                 <dt>Medicine</dt>
-                <dd></dd>
+                <dd>{{ originalDrugName }}</dd>
                 <dt>Route</dt>
-                <dd></dd>
+                <dd>{{ originalRoute }}</dd>
                 <dt>24hr dose</dt>
-                <dd></dd>
+                <dd>{{ original24HDose }} {{ doseUnit }}</dd>
                 <dt>Original duration</dt>
                 <dd></dd>
             </dl>
             <h4>WEANING MEDICINE</h4>
             <dl>
                 <dt>Medicine</dt>
-                <dd></dd>
+                <dd>{{ drug }}</dd>
                 <dt>Route</dt>
-                <dd></dd>
+                <dd>{{ routeRegular }}</dd>
                 <dt>24hr dose</dt>
-                <dd></dd>
+                <dd>{{ startWean24Hr }} {{ doseUnit }}</dd>
                 <dt>Frequency</dt>
-                <dd>Q {{}} H</dd>
+                <dd>Q {{ qHourly }} H</dd>
                 <dt>Start dose</dt>
-                <dd></dd>
+                <dd>{{ startWeanDose }} {{ doseUnit }}</dd>
                 <dt>Rescue Dose</dt>
-                <dd></dd>
+                <dd>{{ startWeanDose }} {{ doseUnit }}</dd>
             </dl>
         </div>
         <table>
@@ -93,20 +93,24 @@
 <script lang="ts">
 import 'reflect-metadata';
 import { Component, Prop, Vue, Inject, Watch } from 'vue-property-decorator';
-import { syringeRounding } from '@/services/infusion-calculations/';
+import { roundToFixed } from '@/services/infusion-calculations/';
 
 type vueNumber = number | '';
 type route = 'po/ng' | 'iv' | 'patch' | 'subcut';
 type doseUnits = 'mg' | 'microg';
 
-interface weanDay { weanDate: Date; regularDose: number; rescueDose: number; id: Symbol }
-
 @Component
 export default class WithdrawalTable extends Vue {
     @Prop({required: true})
-    public drugName!: number;
+    public drug!: string;
+    @Prop({default: ''})
+    private originalDrug!: string;
+    @Prop({ default: 'iv' })
+    public originalRoute!: route;
     @Prop({required: true})
-    public start24HDose!: number;
+    public original24HDose!: number;
+    @Prop({default: 1})
+    public bioavailability!: number;
     @Prop({required: true})
     public weanOverDays!: number;
     @Prop({default: 6})
@@ -117,43 +121,74 @@ export default class WithdrawalTable extends Vue {
     public routeRescue!: route;
     @Prop({default: 'mg'})
     public doseUnit!: doseUnits;
-    @Prop({default: null})
-    public weanOdd!: boolean | null;
+    @Prop({default: false})
+    public weanAlternateDays!: boolean;
 
-    public weanRegime: weanDay[] = [];
-
-    public created() {
-        const dt = new Date();
-        const dayDivisor = this.weanOdd === null
-            ? this.weanOverDays
-            : (this.weanOverDays / 2);
-        const startingDose = syringeRounding(this.start24HDose * this.qHourly / 24);
-        const weanIncrementDaily = startingDose / dayDivisor;
-        for (let i=0; i < dayDivisor; i++) {
-            const weanDate = new Date(dt);
-            weanDate.setDate(weanDate.getDate() + i);
-            this.weanRegime.push({
-                id: Symbol(),
-                regularDose: syringeRounding(startingDose - i * weanIncrementDaily),
-                weanDate,
-                rescueDose: startingDose,
-            });
-        }
-        if (this.weanOdd !== null) {
-            this.weanRegime.reduce((prev, wd, indx) => {
-                    if (prev.length > 0) {
-                        wd.weanDate.setDate(prev[prev.length - 1].weanDate.getDate() + 1);
-                    }
-                    const next = Object.assign({}, wd);
-                    next.weanDate = new Date(next.weanDate);
-                    next.weanDate.setDate(next.weanDate.getDate() + 1);
-                    prev.push(wd, next);
-                    return prev;
-                }, 
-                this.weanOdd ? this.weanRegime.slice(0,1) : []);
-        }
+    public get originalDrugName() {
+        return this.originalDrug || this.drug;
     }
 
+    public get startWean24Hr() {
+        return roundToFixed(this.original24HDose * this.bioavailability);
+    }
+
+    public get startWeanDose() {
+        return roundToFixed(this.startWean24Hr * this.qHourly / 24);
+    }
+
+    public get weanRegime(): WeanDay[] {
+        const dt = new Date();
+        dt.setHours(0, 0, 0, 0);
+        let returnVar = [] as WeanDay[];
+        const dayDivisor = this.weanAlternateDays
+            ? Math.ceil(this.weanOverDays / 2)
+            : this.weanOverDays;
+        const weanIncrement = this.startWeanDose / dayDivisor;
+        for (let i = 0; i < dayDivisor; i++) {
+            const wean = new WeanDay(new Date(dt), 
+                                     this.startWeanDose - i * weanIncrement, 
+                                     this.startWeanDose);
+            wean.addDays(i);
+            returnVar.push(wean);
+        }
+        if (this.weanAlternateDays){
+            const isEven = this.weanOverDays % 2 === 0
+            // 6 over 6 days
+            // odd 6 4 4 2 2
+            // even 6 6 4 4 2 2
+            // date 1 2 3 4
+            // even 1,0->1 . 2,1->3 . 3,2->5 . 4,3->7
+            // odd 1,0->1 2,1->2 . 3,2->4 . 4,3->6
+            returnVar = returnVar.reduce((prev, wd, indx) => {
+                prev.push(wd);
+                const addDays = indx - (isEven ? 0 : 1);
+                if (addDays >= 0) {
+                    wd.addDays(indx);
+                    prev.push(wd.cloneForTomorrow());
+                }
+                return prev;
+            }, [] as WeanDay[]);
+        }
+        return returnVar;
+    }
+}
+
+class WeanDay {
+    constructor (public readonly weanDate: Date,
+                 public regularDose: number,
+                 public rescueDose: number) {
+        this.regularDose = roundToFixed(this.regularDose);
+    }
+    public get id() {
+        return this.weanDate.getTime();
+    }
+    public addDays(days: number) {
+        this.weanDate.setDate(this.weanDate.getDate() + days);
+    }
+    public cloneForTomorrow(): WeanDay {
+        return new WeanDay(new Date(this.weanDate), this.regularDose, this.rescueDose);
+        this.addDays(1);
+    }
 }
 </script>
 
