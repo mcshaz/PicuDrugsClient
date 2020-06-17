7ac644907dced0f61ab9a263ddfc9406f2e8fd67
Brent McSharry <2456704+mcshaz@users.noreply.github.com>
2020-06-17 15:55:02 +1200

    automate github pages deploy

--- scripts/gh-pages-deploy.mjs
+++ scripts/gh-pages-deploy.mjs
@@ -2,6 +2,17 @@ import execa from 'execa';
 import { promises } from 'fs';
 
 (async () => {
+    try {
+        await execa("git", ["update-index", "--refresh"]); 
+        const { stdout } = await execa("git", ["diff-index", "HEAD"]);
+        if (stdout) {
+            console.log("Please stash or commit changes first!");
+            process.exit(1);
+        }
+    } catch (e) {
+        console.log(e.message);
+        process.exit(1);
+    }
     const configFilePath = "vue.config.js";
     const fileOpts = {encoding: 'utf-8'};
     let config = await promises.readFile(configFilePath, fileOpts);
@@ -10,7 +21,7 @@ import { promises } from 'fs';
     try {
         await execa("git", ["checkout", "--orphan", "gh-pages"]);
         console.log("Building...");
-        await execa("yarn", ["run", "build"]);
+        await execa("yarn", ["run", "build-modern"]);
         // Understand if it's dist or build folder
         const folderName = fs.existsSync("dist") ? "dist" : "build";
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
