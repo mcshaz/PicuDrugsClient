0f4a076856cd355040b25866fd0a9e6db3067efd
Brent McSharry <2456704+mcshaz@users.noreply.github.com>
2020-06-18 21:05:54 +1200

    Update dateHelpers.ts

--- src/services/utilities/dateHelpers.ts
+++ src/services/utilities/dateHelpers.ts
@@ -1,4 +1,5 @@
 import { languages } from '@/services/utilities/localisation';
+import { stringify } from 'querystring';
 // note in current form will intentionally not parse dates < 1000 AD
 export function parseDateUtc0(yyyy: string, mm: string, dd: string) {
   const yr = Number(yyyy);
@@ -37,8 +38,6 @@ const shortFormatter = new Intl.DateTimeFormat(languages as string[],
     day: 'numeric',
   });
 
-const doesformatterAdd8206 = shortFormatter.format(0).split('').length > 5; // weird IE thing gives charCode 8206 as 1st element
-
 function remove8206(txt: string) {
   let returnVar = '';
   for (let i = 0; i < txt.length; ++i) {
@@ -49,6 +48,30 @@ function remove8206(txt: string) {
   return returnVar;
 }
 
+let doesformatterAdd8206!: boolean; // weird IE thing gives charCode 8206 as 1st element
+
+let dateOrder: string[];
+if (typeof shortFormatter.formatToParts === 'function') {
+  doesformatterAdd8206 = false;
+  dateOrder = shortFormatter.formatToParts(0)
+    .map((p) => p.type === 'literal' ? p.value : p.type, [] as string[]);
+} else { // not supported ie11 - this is a hack which will only support {calendar: 'gregory', numberingSystem: 'latn'};
+  const egDate = new Date(1984, 11, 30);
+  const formatted = shortFormatter.format(egDate);
+  doesformatterAdd8206 = formatted.indexOf(String.fromCharCode(8206)) !== -1;
+  dateOrder = formatted.replace(egDate.getFullYear().toString(), 'year')
+    .replace((egDate.getMonth() + 1).toString(), 'month')
+    .replace(egDate.getDate().toString(), 'day')
+    .match(/(year|month|day|\W+)/g)!
+    .reduce((accum, txt) => {
+      txt = remove8206(txt);
+      if (txt) {
+        accum.push(txt);
+      }
+      return accum;
+    }, [] as string[]);
+}
+
 export function fixIE11Format(dt: Date | null, formatter = shortFormatter) {
   if (!dt) {
     return '';
@@ -57,25 +80,6 @@ export function fixIE11Format(dt: Date | null, formatter = shortFormatter) {
     return remove8206(formatter.format(dt));
   }
   return formatter.format(dt);
-}
-
-let dateOrder: string[];
-if (typeof shortFormatter.formatToParts === 'function') {
-  dateOrder = shortFormatter.formatToParts(0)
-    .map((p) => p.type === 'literal' ? p.value : p.type, [] as string[]);
-} else { // not supported ie11 - this is a hack which will only support {calendar: 'gregory', numberingSystem: 'latn'};
-  const egDate = new Date(1984, 11, 30);
-  dateOrder = shortFormatter.format(egDate).replace(egDate.getFullYear().toString(), 'year')
-    .replace((egDate.getMonth() + 1).toString(), 'month')
-    .replace(egDate.getDate().toString(), 'day')
-    .match(/(year|month|day|\W+)/g)!
-    .reduce((accum, txt) => {
-      const rv = remove8206(txt);
-      if (rv) {
-        accum.push(rv);
-      }
-      return accum;
-    }, [] as string[]);
 }
 
 export { dateOrder, shortFormatter };
